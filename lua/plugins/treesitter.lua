2b1551f068b1b93dfff1de98ba5f0860cf9ae59e
Ben Kirschniak <ben@kirschniak.de>
2025-06-29 13:14:13 +0200

    removed note-taking related plugins because I felt they were intrusive

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,4 @@ return {
     build = ":TSUpdate",
     ft = LSP_FT,
     config = function() require "plugins.config.treesitter" end,
-    dependencies = {
-        "OXY2DEV/markview.nvim",
-    }
 }
